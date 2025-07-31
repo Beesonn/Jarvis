c32f800e3a8e38b60cf48a9b8d20ddf56b8aa805
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-07-31 16:50:51 +0530

    Update chat.py

--- bot/chat.py
+++ bot/chat.py
@@ -1,8 +1,8 @@
-from text import text
+from jarvis import get_response
 
 async def chat(update, context):
     m = update.message
-    response = text(m)        
+    response = get_response(m)        
     if context.bot.username in m.text.lower():
         await m.reply_text(response.replace("**", "*"), parse_mode="markdown")    
     if m.chat.type != "private"         

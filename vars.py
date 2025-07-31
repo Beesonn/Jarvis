38173d607b770b4fb7243b2b2c4e521ee08c95d0
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-07-31 16:36:56 +0530

    Create vars.py

---
+++ vars.py
@@ -0,0 +1,7 @@
+import os
+
+API_KEY = os.environ.get("MANGO_API_KEY") # You can get your API key from the Telegram bot @MangoAPIBot using the /mykey command.
+
+TOKEN = os.environ.get("TOKEN")
+
+DB_URL = os.environ.get("DB_URL")

3aba104a8ff50af8e311d83a6ff3668dcedf1a5e
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-07-31 18:33:32 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from vars import TOKEN
+from bot import *
+
+
+def main():
+    application = ApplicationBuilder().token(TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat, block=False))
+    application.run_polling()
+  
+main()

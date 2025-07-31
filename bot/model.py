31f3570a7e069cfaf124f9d44c95c8780aa2d46e
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-07-31 10:59:39 +0530

    Update model.py

--- bot/model.py
+++ bot/model.py
@@ -1,8 +1,19 @@
 from mango import Mango
+from database import *
 
 def ai_response(model: str = "gpt-4o", messages: list = []):
     client = Mango(api_key="")
   
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages
+    )
+
+    return response
+
+def ai_chat(m):
+    client = Mango(api_key="")
+    
     response = client.chat.completions.create(
         model=model,
         messages=messages

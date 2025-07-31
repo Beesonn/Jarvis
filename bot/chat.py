de6f504f0ac2ca9df25f2c75692a076bf09546c6
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-07-31 16:58:23 +0530

    Update chat.py

--- bot/chat.py
+++ bot/chat.py
@@ -1,10 +1,9 @@
 from jarvis import get_response
+from telegram import Update
+from telegram.ext import ContextTypes
 
-async def chat(update, context):
+async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     m = update.message
     response = get_response(m)        
-    if context.bot.username in m.text.lower():
-        await m.reply_text(response.replace("**", "*"), parse_mode="markdown")    
-    if m.chat.type != "private"         
-        await m.reply_text(response.replace("**", "*"), parse_mode="markdown")    
-    
+    if m.reply_to_message and m.reply_to_message.from_user.id == context.bot.id or context.bot.username.lower() in m.text.lower() or m.chat.type != "private":
+        await m.reply_text(response.replace("**", "*"), parse_mode="markdown")                

643e7629ceae2ef4cdf7949921b12516533cc5bd
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-08-09 15:07:52 +0530

    Update chat.py

--- bot/chat.py
+++ bot/chat.py
@@ -83,11 +83,11 @@ IMPORTANT: Make botapi markdown can parse like response
                 ]
             }
         ] 
-        response = await get_response(payload, "gpt-5-chat")
+        response = await get_response(payload, "deepseek-v3")
         messages.append({"role": "user", "content": input})   
     else:
         messages.append({"role": "user", "content": m.text})   
-        response = await get_response([{"role": "system", "content": SYSTEM_PROMPT}]+messages, "gpt-5-chat")        
+        response = await get_response([{"role": "system", "content": SYSTEM_PROMPT}]+messages, "deepseek-v3")        
                         
     messages.append({"role": "assistant", "content": response})
     
